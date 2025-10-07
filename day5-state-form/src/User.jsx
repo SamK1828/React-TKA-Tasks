fbf0a807b56f45775d4422c528e2db859d305c14
SamK1828 <kalegaonkarsamarth@gmail.com>
2025-10-07 15:36:26 +0530

    To Do App Project Commit

---
+++ day5-state-form/src/User.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function User() {
+
+    let user = { firstName: "", lastName: "" , email: "", password: ""};
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default User
