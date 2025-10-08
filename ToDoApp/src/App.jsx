3345677353b985cff6f9073485ce525637a06817
SamK1828 <kalegaonkarsamarth@gmail.com>
2025-10-08 23:07:48 +0530

    Modified Main Css and Jsx File

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import StudentForm from './components/StudentForm';
+import StudentTable from './components/StudentTable';
 
 // function App() {
 //   const [count, setCount] = useState(0)
