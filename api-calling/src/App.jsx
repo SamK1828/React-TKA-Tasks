e83680bcfa6397ef8a3fb1b74df93bbe9543c1ce
SamK1828 <kalegaonkarsamarth@gmail.com>
2025-10-14 02:18:45 +0530

    Dummy API Called from App and shown the Data

--- api-calling/src/App.jsx
+++ api-calling/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
 
+
+  // API URL
+  //https://dummyjson.com/todos
+  const fetchData = () => {
+    fetch('https://dummyjson.com/todos')
+      .then((response) => response.json())
+      .then((data) => setTodos(data.todos))
+      .catch((error) => console.log(error))
+  }
+  useEffect(() => {
+    fetchData();
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Todo</th>
+            <th>Completed</th>
+            <th>User ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            todos.map((rec) => (
+              <tr key={rec.id}>
+                <td>{rec.id}</td>
+                <td>{rec.todo}</td>
+                <td>{rec.completed?'Yes':'No'}</td>
+                <td>{rec.userId}</td>
+              </tr>
+            )
+            )
+          }
+        </tbody>
+
+
+      </table>
     </>
   )
 }
